65469254eba8a0834ac2ac7592cefc648c21df5d
Алена Бирюкова <pullshadow@bk.ru>
2025-12-11 19:08:47 +0500

    Добавить вывод ветки в ProgressDemo

--- src/main/java/ru/mentee/power/ProgressDemo.java
+++ src/main/java/ru/mentee/power/ProgressDemo.java
@@ -2,7 +2,7 @@ package ru.mentee.power;
 
 public class ProgressDemo {
     public static void main(String[] args) {
-        // напиши new MenteeProgress( "Имя студента", 1, 6)  затем выдели и набери Ctrl + Alt + V (Windows/Linux) или Option + Command + V (macOS) выделяет выражение в переменную.
+
         MenteeProgress progress = new MenteeProgress("Алена", 2, 6);
         System.out.println(progress.summary());
         if (progress.readyForSprint()) {
@@ -10,5 +10,6 @@ public class ProgressDemo {
         } else {
             System.out.println("Status: backlog first");
         }
+        System.out.println("Current branch: feature/DVT-3");
     }
 }
