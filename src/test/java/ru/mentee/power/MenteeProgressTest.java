13d9c48852ade0b51d106ccce60e6a48c3b9cec2
Алена Бирюкова <pullshadow@bk.ru>
2025-12-12 16:50:35 +0500

    Обновить данные тестов

--- src/test/java/ru/mentee/power/MenteeProgressTest.java
+++ src/test/java/ru/mentee/power/MenteeProgressTest.java
@@ -8,23 +8,23 @@ class MenteeProgressTest {
 
     @Test
     void shouldFormatSummary_whenProgressCreated() {
-        MenteeProgress progress = new MenteeProgress("Ирина", 1, 8);
+        MenteeProgress progress = new MenteeProgress("Алена", 2, 8);
 
         String result = progress.summary();
 
-        assertThat(result).isEqualTo("Sprint 1 -> Ирина: planned 8 h");
+        assertThat(result).isEqualTo("Sprint 1 -> Алена: planned 8 h");
     }
 
     @Test
     void shouldDetectReadiness_whenHoursAboveThreshold() {
-        MenteeProgress progress = new MenteeProgress("Ирина", 1, 4);
+        MenteeProgress progress = new MenteeProgress("Алена", 2, 4);
 
         assertThat(progress.readyForSprint()).isTrue();
     }
 
     @Test
     void shouldDetectLackOfReadiness_whenHoursBelowThreshold() {
-        MenteeProgress progress = new MenteeProgress("Ирина", 1, 2);
+        MenteeProgress progress = new MenteeProgress("Алена", 2, 2);
 
         assertThat(progress.readyForSprint()).isFalse();
     }
